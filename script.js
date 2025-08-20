694fba5d16197d2c0e00dab25d09f14041578adb
Dmitry Kozlov <kodm1986@gmail.com>
2025-08-20 16:58:10 +0300

    Lesson 01 done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("HELLO");
+let title = "Заголовок";
+const screens = 500;
+let screenPrice = 121;
+let rollback = 45121;
+let fullPrice = 4000;
+let adaptive = "адаптация";
+
+alert("Привет! Это первый урок по JS!");
+
+console.log("И сюда мы выведем сообщение об уроке!");
