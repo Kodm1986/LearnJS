df9d455cce4eeba51cddfd9843c6e1caf0b396db
Dmitry Kozlov <kodm1986@gmail.com>
2025-08-26 14:45:38 +0300

    выполнен урок №2

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let title = "Мой первый проект на JS";
 const screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 121;
-let rollback = 65;
+let screenPrice = 3565;
+let rollback = 20;
 let fullPrice = 200000;
 let adaptive = true;
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+screens.toLowerCase();
+console.log(screens.split(", "));
+console.log("Процент отката посреднику за работу " + fullPrice * (rollback / 100) + " рублей");
