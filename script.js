008726b79db6580cf0142bf7b233af8ab9369918
Dmitry Kozlov <kodm1986@gmail.com>
2025-08-26 14:28:36 +0300

    присвоил знаечения переменным

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("HELLO");
+let title = "Мой первый проект на JS";
+const screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 121;
+let rollback = 65;
+let fullPrice = 200000;
+let adaptive = true;
+
